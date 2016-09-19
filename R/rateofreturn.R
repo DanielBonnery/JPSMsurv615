b83243682f5681de65dab16dcaced509e6dd67b0
DanielBonnery <daniel.bonnery@gmail.com>
2016-09-19 15:39:18 -0400

    a

---
+++ R/rateofreturn.R
@@ -0,0 +1,14 @@
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the rate of return of share of different companies. The variables are as follows:
+#'
+#' \itemize{
+#'   \item Sector. Sector ("Oil" or "Service")
+#'   \item rateofreturn. Rate of return
+#'   ...
+#' }
+#'
+#' @format A data frame with 18 rows and 2 variables
+#' @source imaginary
+"rateofreturn"
+#> [1] "diamonds"
