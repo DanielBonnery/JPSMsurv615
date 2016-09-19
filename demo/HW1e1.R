cf9357a4b320bce0e683551de008f9189294d89c
DanielBonnery <daniel.bonnery@gmail.com>
2016-09-19 18:45:12 -0400

    a

--- demo/HW1e1.R
+++ demo/HW1e1.R
@@ -1,5 +1,2 @@
-names(rateofreturn)[2]<-"Rate"
-rateofreturn$Sector<-as.factor(rateofreturn$Sector)
-#save(rateofreturn,file="data/rateofreturn.rdata")
 data("rateofreturn")
 anova(  lm(Rate~Sector,data=rateofreturn)  )
