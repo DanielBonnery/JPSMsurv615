f201b7bbb54e49a4784e3eff29bcab5dd9781cb8
DanielBonnery <daniel.bonnery@gmail.com>
2016-09-26 23:23:25 -0400

    a

--- R/datasetsdesc.R
+++ R/datasetsdesc.R
@@ -30,3 +30,17 @@
 #productivity.improvement<-data.frame(  level=as.factor(rep(c("Low","Moderate","High"),c(9,12,6))),  improvement=c(7.6,8.2,6.8,5.8,6.9,6.6,6.3,7.7,6.0,6.7,8.1,9.4,8.6,7.8,7.7,8.9,7.9,8.3,8.7,7.1,8.4,8.5,9.7,10.2,7.8,9.6,9.5));save(productivity.improvement,file="data/productivity.improvement.Rdata")
 
 
+
+#' Forbes' data on boiling point and  barometric pressure.
+#'
+#' Forbes' data giving boiling point (F) and log of barometric pressure (log of inches of mercury) for 17 locations in the Alps and in Scotland. The variables are as follows:
+#'
+#' \itemize{
+#'   \item boiling.point. Boiling point in F.
+#'   \item log.pressure. Log of the pressure.
+#' }
+#'
+#' @format A data frame with 17 rows and 2 variables
+#' @source Applied Linear Regression by Sanford Weisberg
+"forbes"
+
